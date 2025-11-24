ad8e2711545e520c0d771660eb278b22f537d069
Jesse Breno <107219401+brenotaio@users.noreply.github.com>
2025-11-24 12:04:51 -0300

    create_table.sql

---
+++ create_table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Descarga (
+    id number primary key,
+    dt_venda date,
+    cd_cliente number(5),
+    ds_cliente varchar2(70),
+    cd_produto number(6),
+    ds_produto varchar2(70),
+    email varchar2(100)
+);
+
+
+
+
+
