c82256ea33cac0601c098360d822552e8215a858
Jesse Breno <107219401+brenotaio@users.noreply.github.com>
2025-11-24 12:06:41 -0300

    Delete create_trigger.sql

--- create_trigger.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE TRIGGER tgr tgr_id_descarga
-BEFORE INSERT INTO  Descarga
-FOR EACH ROW
-BEGIN 
-    IF:NEW.id IS NULL THEN
-    :NEW.id: := sq_descarga.NEXTVAL;
-    END IF;
-END;
-/
