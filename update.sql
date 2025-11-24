fa4b6f31b91125b12f28c5b03c2bbbc5cefa0d94
Jesse Breno <107219401+brenotaio@users.noreply.github.com>
2025-11-24 13:37:17 -0300

    Create update.sql

---
+++ update.sql
@@ -0,0 +1,9 @@
+UPDATE Descarga
+SET
+    dt_venda = DATE '2025-05-15',
+    cd_cliente = 54321,
+    ds_cliente = 'Jesse Ferreira',
+    cd_produto = 123456,
+    ds_produto = 'Bike',
+    email = 'jebreno@gmail.com'
+WHERE id = 1;
